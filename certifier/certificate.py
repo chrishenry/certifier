ae77e49ab61f41ba70ded6f4752bb6d726df7095
Chris Henry <henry.christopher@gmail.com>
2016-02-04 15:22:03 -0500

    chg: Allow all errors to bubble up from OpenSSL library

--- certifier/certificate.py
+++ certifier/certificate.py
@@ -19,49 +19,44 @@ def pyopenssl_check_callback(connection, x509, errnum, errdepth, ok):
 
 def get_expiry(host, port=443):
 
-    try:
+    # Check the DNS name
+    socket.getaddrinfo(host, port)[0][4][0]
 
-        # Check the DNS name
-        socket.getaddrinfo(host, port)[0][4][0]
-
-        # Connect to the host and get the certificate, keep a short timeout for
-        #   infrastructure that we inevitably won't have access to.
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.settimeout(2)
-        sock.connect((host, port))
+    # Connect to the host and get the certificate, keep a short timeout for
+    #   infrastructure that we inevitably won't have access to.
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.settimeout(2)
+    sock.connect((host, port))
 
 
-        # Set up the SSL context
-        ctx = SSL.Context(SSL.TLSv1_METHOD)
-        verify_retval = ctx.set_verify(SSL.VERIFY_PEER | SSL.VERIFY_FAIL_IF_NO_PEER_CERT,
-                       pyopenssl_check_callback)
-        ctx.set_options(SSL.OP_NO_SSLv2)
+    # Set up the SSL context
+    ctx = SSL.Context(SSL.TLSv1_METHOD)
+    verify_retval = ctx.set_verify(SSL.VERIFY_PEER | SSL.VERIFY_FAIL_IF_NO_PEER_CERT,
+                   pyopenssl_check_callback)
+    ctx.set_options(SSL.OP_NO_SSLv2)
 
-        # Make the connection get the cert
-        ssl_sock = SSL.Connection(ctx, sock)
-        ssl_sock.setblocking(1)
-        ssl_sock.set_connect_state()
-        ssl_sock.set_tlsext_host_name(host)
-        ssl_sock.do_handshake()
+    # Make the connection get the cert
+    ssl_sock = SSL.Connection(ctx, sock)
+    ssl_sock.setblocking(1)
+    ssl_sock.set_connect_state()
+    ssl_sock.set_tlsext_host_name(host)
+    ssl_sock.do_handshake()
 
-        x509 = ssl_sock.get_peer_certificate()
+    x509 = ssl_sock.get_peer_certificate()
 
-        # print '********************************'
-        # print dir(x509)
-        # print x509.get_notAfter()
-        # print x509.get_subject()
-        # print x509.get_issuer()
-        # print x509.has_expired()
-        # print x509.get_signature_algorithm()
+    # print '********************************'
+    # print dir(x509)
+    # print x509.get_notAfter()
+    # print x509.get_subject()
+    # print x509.get_issuer()
+    # print x509.has_expired()
+    # print x509.get_signature_algorithm()
 
-        if x509.has_expired():
-            raise CertifierException(host, "Cert is expired!")
+    if x509.has_expired():
+        raise CertifierException(host, "Cert is expired!")
 
-        # Pull the certs `notAfter` date into a datetime object
-        expire_date = datetime.strptime(x509.get_notAfter(), "%Y%m%d%H%M%SZ")
-        ssl_sock.shutdown()
+    # Pull the certs `notAfter` date into a datetime object
+    expire_date = datetime.strptime(x509.get_notAfter(), "%Y%m%d%H%M%SZ")
+    ssl_sock.shutdown()
 
-        return expire_date
-
-    except SSL.Error as e:
-        raise
+    return expire_date
